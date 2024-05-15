f265f569a1cfcfef53951661ca30be5617b5e097
pro-grain <owen1024.ee12@nycu.edu.tw>
2024-05-15 14:00:38 +0800

    Update rand_gen.v

--- rand_gen.v
+++ rand_gen.v
@@ -5,4 +5,19 @@ module rand_gen(
     input rst_n,
     output [4:0] rand
     );
+
+    reg [4:0] count_up = 5'b00001;
+
+    initial begin
+        rand = 0;
+    end
+
+    always@(posedge clk)begin
+        count_up = (count_up == 5'b11110)? 5'b00001 : count_up + 1;
+    end
+
+    always@(count_up)begin
+        rand = count_up;
+    end
+
 endmodule
