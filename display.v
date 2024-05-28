8fea078ed01511d973fb23c8d67ca7493a8d98f2
pro-grain <owen1024.ee12@nycu.edu.tw>
2024-05-28 14:58:50 +0800

    Update display.v

--- display.v
+++ display.v
@@ -32,52 +32,59 @@ module display(
     always@(posedge nclk) begin
         if (!rst_n) begin
             count <= 3'b000;
-            segment <= 8'b11111111;
-            enable <= 8'b11111111;
         end else begin
             count <= (count == 3'b111) ? 3'b000 : count + 1;
         end
     end
 
     always@(*) begin
-        case(count)
-            3'b000: enable = 8'b11111110;
-            3'b001: enable = 8'b11111101;
-            3'b010: enable = 8'b11111011;
-            3'b011: enable = 8'b11110111;
-            3'b100: enable = 8'b11101111;
-            3'b101: enable = 8'b11011111;
-            3'b110: enable = 8'b10111111;
-            3'b111: enable = 8'b01111111;
-            default: enable = 8'b11111111;
-        endcase
+        if (!rst_n)begin
+            enable <= 8'b11111111;
+        end else begin 
+            case(count)
+                3'b000: enable <= 8'b11111110;
+                3'b001: enable <= 8'b11111101;
+                3'b010: enable <= 8'b11111011;
+                3'b011: enable <= 8'b11110111;
+                3'b100: enable <= 8'b11101111;
+                3'b101: enable <= 8'b11011111;
+                3'b110: enable <= 8'b10111111;
+                3'b111: enable <= 8'b01111111;
+                default: enable <= 8'b11111111;
+            endcase
+        end
 
         case(count)
-            3'b000: current_digit = display0;
-            3'b001: current_digit = display1;
-            3'b010: current_digit = display2;
-            3'b011: current_digit = display3;
-            3'b100: current_digit = display4;
-            3'b101: current_digit = display5;
-            3'b110: current_digit = display6;
-            3'b111: current_digit = display7;
+            3'b000: current_digit <= display0;
+            3'b001: current_digit <= display1;
+            3'b010: current_digit <= display2;
+            3'b011: current_digit <= display3;
+            3'b100: current_digit <= display4;
+            3'b101: current_digit <= display5;
+            3'b110: current_digit <= display6;
+            3'b111: current_digit <= display7;
+            default: current_digit <= 4'b1111;
         endcase
     end
 
     always@(*) begin
-        case(current_digit)
-            4'd0: segment = 8'b00000011;
-            4'd1: segment = 8'b10011111;
-            4'd2: segment = 8'b00100101;
-            4'd3: segment = 8'b00001101;
-            4'd4: segment = 8'b10011001;
-            4'd5: segment = 8'b01001001;
-            4'd6: segment = 8'b01000001;
-            4'd7: segment = 8'b00011111;
-            4'd8: segment = 8'b00000001;
-            4'd9: segment = 8'b00001001;
-            default: segment = 8'b11111111;
-        endcase
+        if (!rst_n) begin
+            segment <= 8'b11111111;
+        end else begin
+            case(current_digit)
+                4'd0: segment <= 8'b00000011;
+                4'd1: segment <= 8'b10011111;
+                4'd2: segment <= 8'b00100101;
+                4'd3: segment <= 8'b00001101;
+                4'd4: segment <= 8'b10011001;
+                4'd5: segment <= 8'b01001001;
+                4'd6: segment <= 8'b01000001;
+                4'd7: segment <= 8'b00011111;
+                4'd8: segment <= 8'b00000001;
+                4'd9: segment <= 8'b00001001;
+                default: segment <= 8'b11111111;
+            endcase
+        end
     end
 
 endmodule
